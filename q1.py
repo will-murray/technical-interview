baf9b8a0e7973ad40b3faf8a85648f1120349e7e
will murray <williammurray29@gmail.com>
2026-03-30 08:18:45 -0700

    initial commit

---
+++ q1.py
@@ -0,0 +1,8 @@
+# complete the implementation of fizzbuzz. 
+# Given an integer n the program should:
+# print fizz if n is an a multiple of 3
+# print buzz if n is a multiple of 5
+# print fizzbuzz if n is a multiple of 3 and 5
+
+def fizzbuzz(n : int):
+    pass
---
+++ q2.py
@@ -0,0 +1,17 @@
+# This question asks you to make a call to the dogsapi. 
+# Implement the get_dog_breeds() function. 
+
+
+import requests
+BASEURL = "https://dogapi.dog/api/v2"
+
+def get_dog_breeds():
+    """
+    Use the GET breeds/ API endpoint at https://dogapi.dog/api/v2/breeds to fetch 100 dog breeds. 
+    https://dogapi.dog/docs/api-v2 is the official documentation. 
+    You are free to use the requests package. 
+    """
+    
+    
+    pass
+
